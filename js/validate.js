bcae79dd58eb42eb6f2f88aac4399273c51d9207
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-03-24 10:32:31 +0530

    Update validate.js

--- js/validate.js
+++ js/validate.js
@@ -3,8 +3,7 @@ jQuery(document).ready(function($) {
 "use strict";
 	//Contact
 	$('form.validateform').submit(function(){
-
-		var f = $(this).find('.field'), 
+                var f = $(this).find('.field'), 
 		ferror = false, 
 		emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
@@ -52,8 +51,7 @@ jQuery(document).ready(function($) {
 
 		    var i = $(this); // current input
 		    var rule = i.attr('data-rule');
-
-		    if( rule != undefined ){
+                    if( rule != undefined ){
 			var ierror=false; // error flag for current input
 			var pos = rule.indexOf( ':', 0 );
 			if( pos >= 0 ){
@@ -100,5 +98,4 @@ jQuery(document).ready(function($) {
 			$(this).html(result);});}});
 				return false;
 	});
-
 });
